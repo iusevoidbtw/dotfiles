10cb4dbad8acfa9bdb9f83c67b65a92a780780d4
iusevoidbtw <probablymyemail@proton.me>
2024-07-20 15:03:57 +0500

    initial commit

---
+++ .config/nvim/init.lua
@@ -0,0 +1,44 @@
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+Plug('catppuccin/nvim', { ['as'] = 'catppuccin' })
+
+vim.g.coq_settings = { auto_start = 'shut-up' }
+Plug('ms-jpq/coq_nvim', { ['branch'] = 'coq' })
+Plug('ms-jpq/coq.artifacts', { ['branch'] = 'artifacts' })
+
+vim.g.chadtree_settings = {
+	theme = {
+		text_colour_set = 'nerdtree_syntax_dark'
+	},
+	keymap = {
+		change_focus = {'l'},
+		change_focus_up = {'h'},
+		new = {'b'}
+	}
+}
+Plug('ms-jpq/chadtree', { ['branch'] = 'chad', ['do'] = 'python3 -m chadtree deps'})
+
+vim.call('plug#end')
+
+vim.cmd('colorscheme catppuccin-mocha')
+
+-- keybinds
+
+local function map(mode, lhs, rhs, opts)
+	local options = { noremap = true, silent = true }
+	if opts then
+		options = vim.tbl_extend('force', options, opts)
+	end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-- quick keybinds
+map('n', '<leader>s', ':w<cr>') -- save
+map('n', '<leader>q', ':qa!<cr>') -- quit
+map('n', '<leader>t', ':CHADopen<cr>') -- open chadtree
+map('n', '<leader>y', ':abo vnew term://yash<cr>') -- open yash
+map('n', '<leader>c', ':nohl<cr>') -- clear search highlighting
+map('n', '<leader>e', ':%s/    /	/g<cr>') -- replace 4 spaces with tabs
---
+++ .profile
@@ -0,0 +1,21 @@
+### --- PATH --
+export PATH="$PATH:$HOME/.local/bin:/opt/diet/bin"
+
+### --- GENERAL CHANGES ---
+
+# avoid loading nvidia modules when not necessary
+# export __EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/50_mesa.json
+
+# set va-api drivers to ihd properly
+export LIBVA_DRIVER_NAME=iHD
+export LIBVA_DRIVERS_PATH=/usr/lib/dri/
+
+# set nvim as the default editor
+export EDITOR=nvim
+
+# use catppuccin for bat and bemenu
+export BAT_THEME="Catppuccin-mocha"
+export BEMENU_OPTS='--fb "#1e1e2e" --ff "#cdd6f4" --nb "#1e1e2e" --nf "#cdd6f4" --tb "#1e1e2e" --hb "#1e1e2e" --tf "#f38ba8" --hf "#f9e2af" --af "#cdd6f4" --ab "#1e1e2e"'
+
+# use wayland for sdl2
+SDL_VIDEODRIVER=wayland
